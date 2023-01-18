a3bfae02b03c0e0bcfa49b41cc8647f778f843f4
Sajjad <faniam321@gmail.com>
2023-01-18 14:32:22 +0330

    fix a typo

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    Login, Register, UserList, UpdateInformations, ChangePassword
+    Login, Register, UserList, UpdateInformation, ChangePassword
 )
 
 app_name = 'users'
@@ -9,6 +9,6 @@ urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('register/', Register.as_view(), name='register'),
     path('list/', UserList.as_view(), name='user-list'),
-    path('list/<int:pk>/', UpdateInformations.as_view(), name='update-info'),
+    path('list/<int:pk>/', UpdateInformation.as_view(), name='update-info'),
     path('change-password/', ChangePassword.as_view(), name='change-password'),
 ]
