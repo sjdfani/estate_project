2fc318adfb2c5b0e88d7866123553056e5820796
Sajjad <faniam321@gmail.com>
2023-01-20 02:15:02 +0330

    increase total size

--- estate/models.py
+++ estate/models.py
@@ -7,19 +7,19 @@ class Buy_Sell_Home(models.Model):
     creator = models.ForeignKey(
         User, on_delete=models.SET_NULL, verbose_name=_('Creator User'), null=True, related_name='buy_sell_home_creator'
     )
-    area_code = models.CharField(max_length=10, verbose_name=_('Area Code'))
-    date = models.CharField(max_length=20, verbose_name=_(
+    area_code = models.CharField(max_length=100, verbose_name=_('Area Code'))
+    date = models.CharField(max_length=200, verbose_name=_(
         'Date'), null=True, blank=True)
     owner_name = models.CharField(
-        max_length=20, verbose_name=_('Owner Name'), null=True, blank=True)
+        max_length=200, verbose_name=_('Owner Name'), null=True, blank=True)
     owner_phone = models.CharField(
         max_length=11, verbose_name=_('Owner Phone'), null=True, blank=True)
     street = models.CharField(
-        max_length=50, verbose_name=_('Street'), null=True, blank=True)
+        max_length=500, verbose_name=_('Street'), null=True, blank=True)
     plaque = models.CharField(
-        max_length=10, verbose_name=_('Plaque'), null=True, blank=True)
+        max_length=100, verbose_name=_('Plaque'), null=True, blank=True)
     floors = models.CharField(
-        max_length=10, verbose_name=_('Floors'), null=True, blank=True)
+        max_length=100, verbose_name=_('Floors'), null=True, blank=True)
     meterage = models.IntegerField(
         default=0, verbose_name=_('Meterage'), null=True, blank=True)
     price_per_meter = models.IntegerField(
@@ -27,23 +27,23 @@ class Buy_Sell_Home(models.Model):
     total_price = models.IntegerField(
         default=0, verbose_name=_('Total Price'), null=True, blank=True)
     customer_name = models.CharField(
-        max_length=20, verbose_name=_('Customer Name'), null=True, blank=True)
+        max_length=200, verbose_name=_('Customer Name'), null=True, blank=True)
     style = models.CharField(
-        max_length=20, verbose_name=_('Style'), null=True, blank=True)
+        max_length=200, verbose_name=_('Style'), null=True, blank=True)
     heating = models.CharField(
-        max_length=20, verbose_name=_('Heating'), null=True, blank=True)
+        max_length=200, verbose_name=_('Heating'), null=True, blank=True)
     bottom = models.CharField(
-        max_length=20, verbose_name=_('Bottom'), null=True, blank=True)
+        max_length=200, verbose_name=_('Bottom'), null=True, blank=True)
     electricity = models.CharField(
-        max_length=20, verbose_name=_('Electricity'), null=True, blank=True)
+        max_length=200, verbose_name=_('Electricity'), null=True, blank=True)
     kitchen = models.CharField(
-        max_length=20, verbose_name=_('Kitchen'), null=True, blank=True)
+        max_length=200, verbose_name=_('Kitchen'), null=True, blank=True)
     faucets = models.CharField(
-        max_length=20, verbose_name=_('Faucets'), null=True, blank=True)
+        max_length=200, verbose_name=_('Faucets'), null=True, blank=True)
     bathtub = models.CharField(
-        max_length=20, verbose_name=_('Bathtub'), null=True, blank=True)
+        max_length=200, verbose_name=_('Bathtub'), null=True, blank=True)
     window = models.CharField(
-        max_length=20, verbose_name=_('Window'), null=True, blank=True)
+        max_length=200, verbose_name=_('Window'), null=True, blank=True)
     description = models.TextField(
         verbose_name=_('Description'), null=True, blank=True)
     status = models.BooleanField(default=False, verbose_name=_('Status Home'))
